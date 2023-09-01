c3b781dcc4b18b865f4effee782c3e16409ae01b
Hasiel Alvarez <hasielhassan@gmail.com>
2023-09-01 03:02:35 -0700

    Improve the compile resources batch script

--- resources/compile_resources.bat
+++ resources/compile_resources.bat
@@ -1,7 +1,12 @@
-C:\Python37\Scripts\pyside2-uic --from-imports manager_form.ui > ..\modules\manager_form.py
-C:\Python37\Scripts\pyside2-uic --from-imports node_details_form.ui > ..\modules\node_details_form.py
-C:\Python37\Scripts\pyside2-uic --from-imports slot_details_form.ui > ..\modules\slot_details_form.py
+pyside2-uic manager_form.ui > ..\modules\manager_form.py
+pyside2-uic node_details_form.ui > ..\modules\node_details_form.py
+pyside2-uic slot_details_form.ui > ..\modules\slot_details_form.py
 
-python -m Qt --convert D:\Development\HasielHassan\PlumberManager\modules\manager_form.py
-python -m Qt --convert D:\Development\HasielHassan\PlumberManager\modules\node_details_form.py
-python -m Qt --convert D:\Development\HasielHassan\PlumberManager\modules\slot_details_form.py
+@powershell -Command "get-content -path ..\modules\manager_form.py | foreach-object {$_ -replace 'from PySide2','from Qt'} | Set-Content ..\modules\manager_form.py.copy"
+@powershell -Command "Move-Item -Path ..\modules\manager_form.py.copy -Destination ..\modules\manager_form.py -force" 
+
+@powershell -Command "get-content -path ..\modules\node_details_form.py | foreach-object {$_ -replace 'from PySide2','from Qt'} | Set-Content ..\modules\node_details_form.py.copy"
+@powershell -Command "Move-Item -Path ..\modules\node_details_form.py.copy -Destination ..\modules\node_details_form.py -force" 
+
+@powershell -Command "get-content -path ..\modules\slot_details_form.py | foreach-object {$_ -replace 'from PySide2','from Qt'} | Set-Content ..\modules\slot_details_form.py.copy"
+@powershell -Command "Move-Item -Path ..\modules\slot_details_form.py.copy -Destination ..\modules\slot_details_form.py -force" 
