5996a5c767b1968f4ac7abd7c8fa7b8689327fb5
Hasiel Alvarez <hasielhassan@gmail.com>
2023-09-05 22:40:51 -0700

    update paths on compile resources

--- resources/compile_resources.bat
+++ resources/compile_resources.bat
@@ -1,12 +1,12 @@
-pyside2-uic manager_form.ui > ..\modules\manager_form.py
-pyside2-uic node_details_form.ui > ..\modules\node_details_form.py
-pyside2-uic slot_details_form.ui > ..\modules\slot_details_form.py
+pyside2-uic manager_form.ui > ..\modules\ui\manager_form.py
+pyside2-uic node_details_form.ui > ..\modules\ui\node_details_form.py
+pyside2-uic slot_details_form.ui > ..\modules\ui\slot_details_form.py
 
-@powershell -Command "get-content -path ..\modules\manager_form.py | foreach-object {$_ -replace 'from PySide2','from Qt'} | Set-Content ..\modules\manager_form.py.copy"
-@powershell -Command "Move-Item -Path ..\modules\manager_form.py.copy -Destination ..\modules\manager_form.py -force" 
+@powershell -Command "get-content -path ..\modules\ui\manager_form.py | foreach-object {$_ -replace 'from PySide2','from Qt'} | Set-Content ..\modules\ui\manager_form.py.copy"
+@powershell -Command "Move-Item -Path ..\modules\ui\manager_form.py.copy -Destination ..\modules\ui\manager_form.py -force" 
 
-@powershell -Command "get-content -path ..\modules\node_details_form.py | foreach-object {$_ -replace 'from PySide2','from Qt'} | Set-Content ..\modules\node_details_form.py.copy"
-@powershell -Command "Move-Item -Path ..\modules\node_details_form.py.copy -Destination ..\modules\node_details_form.py -force" 
+@powershell -Command "get-content -path ..\modules\ui\node_details_form.py | foreach-object {$_ -replace 'from PySide2','from Qt'} | Set-Content ..\modules\ui\node_details_form.py.copy"
+@powershell -Command "Move-Item -Path ..\modules\ui\node_details_form.py.copy -Destination ..\modules\ui\node_details_form.py -force" 
 
-@powershell -Command "get-content -path ..\modules\slot_details_form.py | foreach-object {$_ -replace 'from PySide2','from Qt'} | Set-Content ..\modules\slot_details_form.py.copy"
-@powershell -Command "Move-Item -Path ..\modules\slot_details_form.py.copy -Destination ..\modules\slot_details_form.py -force" 
+@powershell -Command "get-content -path ..\modules\ui\slot_details_form.py | foreach-object {$_ -replace 'from PySide2','from Qt'} | Set-Content ..\modules\ui\slot_details_form.py.copy"
+@powershell -Command "Move-Item -Path ..\modules\ui\slot_details_form.py.copy -Destination ..\modules\ui\slot_details_form.py -force" 
